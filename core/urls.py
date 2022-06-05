548d7baa930563a8578134092cbc87a248865939
Shaheer <shaheerr7@gmail.com>
2022-06-05 06:23:48 +0500

    80% Done

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from core.views import Home, ProfileList
+from core.views import Home, ProfileList, ProfileCreate, Watch
 
 app_name = 'core'
 
 urlpatterns = [
     path('', Home.as_view()),
     path('profile/', ProfileList.as_view(), name='profile_list'),
+    path('profile/create/', ProfileCreate.as_view(), name='profile_create'),
+    path('watch/<str:profile_id>', Watch.as_view(), name='watch'),
 ]
